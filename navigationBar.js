61b98130a787afb35cf063399de8ec164160baf2
1234Louiji <1234louiji@gmail.com>
2025-08-16 06:29:37 +0000

    wdsa

--- navigationBar.js
+++ navigationBar.js
@@ -1,9 +1,9 @@
 var navigation = document.createElement("div")
 const bar = [{name:'intro',function: null},{name:'categories', function: null},{name:'community', function: null}]
-navigation.innerHTML = `<div onclick="location.reload(true)>P-ARTICIPATE</div>"`
+navigation.innerHTML = `<div onclick="location.reload(true)">P-ARTICIPATE</div>`
 for(let i in bar){
     bar[i].element = document.createElement("div")
-    bar[i].element.textContent = bar[i].name.toLocaleUpperCase(0)
+    bar[i].element.textContent = bar[i].name[0].toUpperCase() + bar[i].name.slice(1)
     navigation.appendChild(bar[i].element)
 }
 document.getElementById("navigate").appendChild(navigation)
