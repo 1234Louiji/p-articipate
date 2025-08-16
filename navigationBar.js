d9a84e6f2b781c05ef21b0c690c20b32b4ce3990
1234Louiji <1234louiji@gmail.com>
2025-08-16 06:37:08 +0000

    as

--- navigationBar.js
+++ navigationBar.js
@@ -1,6 +1,6 @@
 var navigation = document.createElement("div")
 const bar = [{name:'intro',function: null},{name:'categories', function: null},{name:'community', function: null}]
-navigation.innerHTML = `<div onclick="window.location.replace(https://1234louiji.github.io/p-articipate/)">P-ARTICIPATE</div>`
+navigation.innerHTML = `<div onclick='window.location.replace("https://1234louiji.github.io/p-articipate/")'>P-ARTICIPATE</div>`
 for(let i in bar){
     bar[i].element = document.createElement("div")
     bar[i].element.textContent = bar[i].name[0].toUpperCase() + bar[i].name.slice(1)
