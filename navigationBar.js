2d43d5288f00d5bf823a572511a4824ac4ce694c
1234Louiji <1234louiji@gmail.com>
2025-08-16 06:19:39 +0000

    ss

--- navigationBar.js
+++ navigationBar.js
@@ -3,8 +3,8 @@ const bar = [{name:'intro',function: null},{name:'categories', function: null},{
 var logo = document.createElement("div")
 logo.onclick = location.reload(true)
 logo.textContent = 'P-ARTICIPATE'
+navigation.appendChild(logo)
 for(let i in bar){
-    console.log(bar[i])
     bar[i].element = document.createElement("div")
     bar[i].element.textContent = bar[i].name.toLocaleUpperCase(0)
     navigation.appendChild(bar[i].element)
